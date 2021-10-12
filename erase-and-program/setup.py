42495644b04f5fafb75d7bbe9ebe7b12deca31fb
root <root@pi-syna>
2021-10-12 10:36:39 -0700

    remove config files in frontend extension

--- erase-and-program/setup.py
+++ erase-and-program/setup.py
@@ -25,11 +25,6 @@ labext_name = "@webds/erase-and-program"
 data_files_spec = [
     ("share/jupyter/labextensions/%s" % labext_name, str(lab_path.relative_to(HERE)), "**"),
     ("share/jupyter/labextensions/%s" % labext_name, str("."), "install.json"),
-    ("etc/jupyter/jupyter_server_config.d",
-     "jupyter-config/server-config", "webds_erase_and_program.json"),
-    # For backward compatibility with notebook server
-    ("etc/jupyter/jupyter_notebook_config.d",
-     "jupyter-config/nb-config", "webds_erase_and_program.json"),
 ]
 
 long_description = (HERE / "README.md").read_text()
