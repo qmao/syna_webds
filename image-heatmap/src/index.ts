41712624867c6bd8a77f348d4c5418d6b1a86a1f
root <root@pi-syna>
2021-12-22 21:50:57 -0800

    can show heatmap but direction not correct

--- image-heatmap/src/index.ts
+++ image-heatmap/src/index.ts
@@ -10,6 +10,8 @@ import { ILauncher } from '@jupyterlab/launcher';
 
 import { Widget } from '@lumino/widgets';
 
+import { requestAPI } from './handler';
+
 import { launcherIcon } from './icons';
 
 import HeatMap from 'heatmap-ts'
@@ -21,34 +23,97 @@ namespace CommandIDs {
   export const get = 'webds:image-heatmap';
 }
 
-function update() {
-	const heatMap = new HeatMap({
+
+const set_report = async (disable: number[], enable: number[]): Promise<string | undefined> => {
+	const dataToSend = {
+		enable: enable,
+		disable: disable
+	};
+	console.log(dataToSend);
+	try {
+		const reply = await requestAPI<any>('report', {
+			body: JSON.stringify(dataToSend),
+			method: 'POST',
+		});
+
+		console.log(reply);
+		return Promise.resolve("ok");
+	} catch (error) {
+		console.log(error);
+		console.log(error.message);
+		return Promise.reject(error.message);
+	}
+}
+
+function matrixIndexed(details: number[][]) {
+	let dataPoints = [];
+	for(let i = 0; i < details.length; i++){
+		// get the length of the inner array elements
+		let innerArrayLength = details[i].length;
+		
+		// looping inner array elements
+		for(let j = 0; j < innerArrayLength; j++) {
+			//console.log(details[i][j]);
+			let dataPoint = {
+			  x: i,
+			  y: j,
+			  value: details[i][j],
+			};
+			dataPoints.push(dataPoint);
+			//console.log(dataPoints);
+		}
+	}
+	return dataPoints;
+}
+
+declare global {
+    var source: EventSource;
+	var heatMap: HeatMap;
+}
+
+const eventHandler = (event: any) => {
+	let report = JSON.parse(event.data);
+	console.log(report)
+	console.log(report.image);
+	
+	update(report.image);
+}
+
+function prepare() {
+	set_report([17], [18]);
+	
+	globalThis.heatMap = new HeatMap({
 	  container: document.getElementById('image_heatmap')!,
 	  maxOpacity: .6,
-	  radius: 50,
-	  blur: 0.90,
+	  radius: 10,
+	  blur: 0.75,
 	})
+	
+	console.log(globalThis.heatMap);
 
-	console.log(heatMap);
+	// set event handler
+	globalThis.source = new window.EventSource('/webds/report');
+	console.log(globalThis.source);
+	if (globalThis.source != null) {
+		globalThis.source.addEventListener('report', eventHandler, false);
+	}
+	else {
+		console.log("event source is null");
+	}
+}
 
+function update(data: number[][]) {
 
-	heatMap.setData({
+	//let data = [[1,2,3,4,5,6,0,0,0,0],[11,22,33,44,55,66,0,1,10,3],[1,2,3,4,5,6,20,0,0,1],[11,22,33,44,55,66,0,100,0,0],
+	//[1,2,30,0,0,0,1,4,5,6],[0,1,20,10,11,22,33,44,55,66]];
+
+	let points = matrixIndexed(data);
+	console.log(points);
+
+	globalThis.heatMap.setData({
 	  max: 100,
 	  min: 1,
-	  data: [
-		{
-		  x: 100,
-		  y: 100,
-		  value: 100,
-		  radius: 20
-		},
-		{
-		  x: 200,
-		  y: 250,
-		  value: 50,
-		  radius: 30
-		}
-	  ]
+	  data: points
 	})
 }
 
@@ -86,8 +151,6 @@ const extension: JupyterFrontEndPlugin<void> = {
           widget.title.label = extension_string;
           widget.title.closable = true;
           widget.title.icon = launcherIcon;
-		  
-		  update();
         }
 
         if (!tracker.has(widget))
@@ -97,6 +160,8 @@ const extension: JupyterFrontEndPlugin<void> = {
           shell.add(widget, 'main');
 
         shell.activateById(widget.id);
+		
+		prepare();
       }
     });
 
@@ -111,6 +176,5 @@ const extension: JupyterFrontEndPlugin<void> = {
 
   }
 };
-
 
 export default extension;
