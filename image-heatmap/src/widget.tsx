934dc0cdb2fe82939f7c0d303d03754874144552
root <root@pi-syna>
2021-12-23 20:23:36 -0800

    add raw/delta select

--- image-heatmap/src/widget.tsx
+++ image-heatmap/src/widget.tsx
@@ -2,7 +2,11 @@ import { ReactWidget } from '@jupyterlab/apputils';
 import React from 'react';
 
 import Box from '@mui/material/Box';
-//import Paper from '@mui/material/Paper';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
 
 //import { ThemeProvider } from "@mui/material/styles";
 //import CssBaseline from '@mui/material/CssBaseline';
@@ -15,28 +19,68 @@ import Box from '@mui/material/Box';
 
 //import webdsTheme from './webdsTheme';
 
+const options = [
+    'Delta',
+    'Raw',
+];
 
+const ITEM_HEIGHT = 20;
 
 export default function MyButton () {
 
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     return (
-        <Box id='mybox' className='mybox'
+        <Box
             sx={{
                 width: 600,
                 height: 400,
-                //display: 'flex',
-                //flexWrap: 'wrap',
-                '& > *': {
-                    m: 2,
-                    p: 2,
-                    position: 'relative',
-                    alignContent: 'center',
-                    alignItems: 'center',
-                    alignSelf: 'center',
-                },
+                flexDirection: "column",
+                display: "flex",
+                alignItems: "flex-start"
             }}
         >
+            <IconButton
+                aria-label="more"
+                id="long-button"
+                aria-controls="long-menu"
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+            >
+                <MoreVertIcon />
+            </IconButton>
 
+            <Menu
+                id="long-menu"
+                MenuListProps={{
+                    'aria-labelledby': 'long-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        maxHeight: ITEM_HEIGHT * 4.5,
+                        width: '20ch',
+                    },
+                }}
+            >
+                {options.map((option) => (
+                    <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                        {option}
+                    </MenuItem>
+                ))}
+            </Menu>
+            <Paper className="mybox" id='mybox' sx={{ width: 300, height: 200 }}></Paper>
         </Box>
     );
 }
