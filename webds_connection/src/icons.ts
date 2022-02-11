b96cb3f0aa77dcb050cb3b529ebbc2035e00921f
qmao <qmaooo@gmail.com>
2022-02-11 19:33:33 +0800

    Update icons.ts

--- webds_connection/src/icons.ts
+++ webds_connection/src/icons.ts
@@ -3,6 +3,6 @@ import { LabIcon } from '@jupyterlab/ui-components';
 import extensionConnectionSvg from '../style/icons/extensionConnection.svg';
 
 export const extensionConnectionIcon = new LabIcon({
-  name: 'webDS:extension-program',
+  name: 'webDS:extension-connection',
   svgstr: extensionConnectionSvg
 });
