6a549320d5c1c9fa3bf12055ae3c4f6b5af45694
root <root@pi-syna>
2022-03-03 02:23:22 -0800

    webds_api: add report_manager.py

---
+++ server-extension/webds_api/report_manager.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append("/usr/local/syna/lib/python")
+import threading
+import time
+from .touchcomm_manager import TouchcommManager
+from threading import Thread
+from threading import Lock
+from time import sleep
+    
+
+class ReportManager(object):
+    _instance = None
+    _counter = 0
+    _thread = None
+    _report = ('timeout', None)
+    ###_lock = Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        print("ReportManager singleton object is created")
+
+
+    def doit(self, arg):
+        t = threading.currentThread()
+        tc = None
+        try:
+            tc = TouchcommManager()
+            while getattr(t, "do_run", True):
+                ###self._lock.acquire()
+                self._report = tc.getReport(1)
+                ###self._lock.release()
+                sleep(0.0001)
+                ###print ("working on %s" % self._counter)
+            print("Stopping as you wish.")
+        except Exception as e:
+            print(tc)
+            if tc is not None:
+                print("report sse disconnect tc")
+                tc.disconnect()
+            raise
+
+    def getReport(self):
+        ###self._lock.acquire()
+        data = self._report
+        self._report = ('timeout', None)
+        ###self._lock.release()
+        return data
+            
+    def setState(self, state):
+        print("Set state:", state)
+        if state:
+            if self._counter is 0:
+                print("Create Report Thread")
+                self._thread = threading.Thread(target=self.doit, args=("task",))
+                self._thread.start()
+            self._counter += 1
+        else:
+            if self._counter > 0:
+                self._counter -= 1
+                if self._counter is 0:
+                    self._thread.do_run = False
